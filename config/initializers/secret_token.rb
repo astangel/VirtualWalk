52c1fb12b1ed226d4c6274fe893e4fb316cf2004
Andrew St. Angelo <astangel@iupui.edu>
2013-04-10 20:53:19 -0400

    VirtualWalk - Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VirtualWalk::Application.config.secret_token = '3b2657785d16e9048fbaba333cfd2d1d203870598ca86ec8dcb049710123d84699c4f834a432b13249d9c6bff7d8a633d3c91deb83593efea9d2ad0fd1843e2e'
