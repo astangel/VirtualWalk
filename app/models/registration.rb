0ec45fa7745390418ce2260be03c743cb7bef5ac
Andrew St. Angelo <astangel@iupui.edu>
2013-04-29 22:21:35 -0400

    VirtualWalk - Added validation to registration

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,8 @@
 class Registration < ActiveRecord::Base
   attr_accessible :personal_goal, :event_id, :user_id, :team_id, :user, :team, :event
+  validates :user, :presence => true
+  validates :event, :presence => true
+  validates :personal_goal, :presence => true
   
   belongs_to :user
   belongs_to :event
